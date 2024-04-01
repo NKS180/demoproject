ac1f1c7d28f318feb6c5ce0917de13452a3dd713
Niraj <nirajsharmam1m@gmail.com>
2024-04-01 10:05:05 +0530

    first python code

---
+++ hello.py
@@ -0,0 +1,10 @@
+def function(n):
+      if n==0:
+      return 1
+      else:
+      return n*factorial(n-1)
+      
+ number=5
+ result =fractorial(number)
+ print("the factorial of {number} is {Result}")
+ 
